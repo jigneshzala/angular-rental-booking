240c3cb58ddf051c878b72c1e56b90f13a73b260
jignesh zala <jigneshkumar1494@gmail.com>
2020-12-14 18:59:14 +0530

    added header component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,8 +3,9 @@ import { NgModule } from "@angular/core";
 
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
+import { HeaderComponent } from './shared/header/header.component';
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, HeaderComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
