2051403b4f73edbdea9bb491331471a8c838b78b
jignesh zala <jigneshkumar1494@gmail.com>
2020-12-15 19:17:34 +0530

    added Rental Module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,12 @@ import { NgModule } from "@angular/core";
 
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
-import { HeaderComponent } from './shared/header/header.component';
-import { RentalDetailComponent } from './rental/rental-detail/rental-detail.component';
-import { RentalListingComponent } from './rental/rental-listing/rental-listing.component';
+import { HeaderComponent } from "./shared/header/header.component";
+
+import { RentalModule } from "./rental/rental.module";
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, RentalDetailComponent, RentalListingComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  declarations: [AppComponent, HeaderComponent],
+  imports: [BrowserModule, AppRoutingModule, RentalModule],
   providers: [],
   bootstrap: [AppComponent],
 })
