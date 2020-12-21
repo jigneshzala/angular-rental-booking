3b12f7ebe8271f3296f0749380b7959d82ef4b49
jignesh zala <jigneshkumar1494@gmail.com>
2020-12-21 21:29:45 +0530

    Get data from DB

--- server/routes/rentals.js
+++ server/routes/rentals.js
@@ -1,19 +1,12 @@
 const express = require('express');
 const router = express.Router();
-
-const {
+const { 
   getRentals,
   getRentalById,
-  createRental,
-  updateRental,
-  deleteRental
-} = require('../controllers/rentals');
-
+  createRental } = require('../controllers/rentals');
 
 router.get('', getRentals);
 router.get('/:rentalId', getRentalById);
 router.post('', createRental);
-router.delete('/:id', deleteRental);
-router.patch('/:id', updateRental);
 
 module.exports = router;
