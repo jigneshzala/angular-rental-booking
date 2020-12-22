3769327e74196bbac8d83727d4d96cdf6d011f2d
jignesh zala <jigneshkumar1494@gmail.com>
2020-12-22 20:43:26 +0530

    Custom Directive validator

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from "@angular/common";
 import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 import { LoginComponent } from "./login/login.component";
 import { RegisterComponent } from "./register/register.component";
+import { ForbiddenEmailDirective } from "../shared/validators/forbidden-email.directive";
 
 @NgModule({
-  declarations: [LoginComponent, RegisterComponent],
+  declarations: [LoginComponent, RegisterComponent, ForbiddenEmailDirective],
   imports: [CommonModule, FormsModule, ReactiveFormsModule],
 })
 export class AuthModule {}
