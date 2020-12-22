9ac5b0bbe8d1e0e2fa493625da77e7ed6b8af5a4
jignesh zala <jigneshkumar1494@gmail.com>
2020-12-22 19:16:26 +0530

    Reactive forms validation

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from "@angular/core";
-import { FormBuilder, FormGroup } from "@angular/forms";
+import {
+  FormBuilder,
+  FormGroup,
+  Validators,
+  AbstractControl,
+} from "@angular/forms";
 
 @Component({
   selector: "app-login",
@@ -8,6 +13,7 @@ import { FormBuilder, FormGroup } from "@angular/forms";
 })
 export class LoginComponent implements OnInit {
   loginForm: FormGroup;
+  emailPattern = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
   constructor(private fb: FormBuilder) {}
 
@@ -17,13 +23,24 @@ export class LoginComponent implements OnInit {
 
   initForm() {
     this.loginForm = this.fb.group({
-      email: [""],
-      password: [""],
+      email: ["", [Validators.required, Validators.pattern(this.emailPattern)]],
+      password: ["", [Validators.required, Validators.minLength(6)]],
     });
   }
 
   login() {
+    if (this.loginForm.invalid) {
+      return;
+    }
+
     alert(this.diagnostic);
+  }
+
+  get email(): AbstractControl {
+    return this.loginForm.get("email");
+  }
+  get password(): AbstractControl {
+    return this.loginForm.get("password");
   }
 
   get diagnostic(): string {
