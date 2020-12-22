d6a6bc106be896bdf3c92e61e519ae50dc5bbcca
jignesh zala <jigneshkumar1494@gmail.com>
2020-12-22 19:05:30 +0530

    Login with Reactive forms

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { FormBuilder, FormGroup } from "@angular/forms";
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  selector: "app-login",
+  templateUrl: "./login.component.html",
+  styleUrls: ["./login.component.scss"],
 })
 export class LoginComponent implements OnInit {
+  loginForm: FormGroup;
 
-  constructor() { }
+  constructor(private fb: FormBuilder) {}
 
   ngOnInit() {
+    this.initForm();
   }
 
+  initForm() {
+    this.loginForm = this.fb.group({
+      email: [""],
+      password: [""],
+    });
+  }
+
+  login() {
+    alert(this.diagnostic);
+  }
+
+  get diagnostic(): string {
+    return JSON.stringify(this.loginForm.value);
+  }
 }
