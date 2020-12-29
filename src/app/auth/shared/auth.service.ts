85188c6493c7a07fdfd28f40662b49722f72debf
jignesh zala <jigneshkumar1494@gmail.com>
2020-12-29 17:58:59 +0530

    Save JWT

--- src/app/auth/shared/auth.service.ts
+++ src/app/auth/shared/auth.service.ts
@@ -4,12 +4,24 @@ import { HttpClient, HttpErrorResponse } from "@angular/common/http";
 import { Observable, throwError } from "rxjs";
 import { catchError, map } from "rxjs/operators";
 import { exctractApiError } from "src/app/shared/helpers/functions";
+import { JwtHelperService } from "@auth0/angular-jwt";
 
+const jwt = new JwtHelperService();
+
+class DecodedToken {
+  exp: number = 0;
+  username: string = "";
+  userId: string = "";
+}
 @Injectable({
   providedIn: "root",
 })
 export class AuthService {
-  constructor(private http: HttpClient) {}
+  private decodedToken: DecodedToken;
+
+  constructor(private http: HttpClient) {
+    this.decodedToken = new DecodedToken();
+  }
 
   //'/api/v1/users/register'
   register(formData: RegisterForm): Observable<any> {
@@ -34,7 +46,15 @@ export class AuthService {
     );
   }
 
-  private saveToken(token) {
-    alert("I am saving token!");
+  private saveToken(token: string): string | null {
+    const decodedToken = jwt.decodeToken(token);
+    if (!decodedToken) {
+      return null;
+    }
+
+    this.decodedToken = decodedToken;
+
+    localStorage.setItem("bwm_auth_token", token);
+    return token;
   }
 }
