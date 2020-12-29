3fd679215d86eabdf94d636b5dcc7dc71910fae9
jignesh zala <jigneshkumar1494@gmail.com>
2020-12-29 20:24:04 +0530

    Finishing auth guard

--- src/app/auth/shared/auth.guard.ts
+++ src/app/auth/shared/auth.guard.ts
@@ -1,9 +1,6 @@
 import { Injectable } from "@angular/core";
-import {
-  CanActivate,
-  ActivatedRouteSnapshot,
-  RouterStateSnapshot,
-} from "@angular/router";
+import { CanActivate, RouterStateSnapshot, Router } from "@angular/router";
+import { AuthService } from "./auth.service";
 
 @Injectable({
   providedIn: "root",
@@ -11,8 +8,38 @@ import {
 export class AuthGuard implements CanActivate {
   private url: string;
 
-  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
+  constructor(private auth: AuthService, private router: Router) {}
+
+  canActivate(_: any, state: RouterStateSnapshot) {
     this.url = state.url;
+    return this.auth.isAuthenticated
+      ? this.handleAuthState()
+      : this.handleNotAuthState();
+  }
+
+  private handleAuthState(): boolean {
+    if (this.isAuthPage) {
+      this.router.navigate(["/rentals"]);
+      return false;
+    }
+
     return true;
+  }
+
+  private handleNotAuthState(): boolean {
+    if (this.isAuthPage) {
+      return true;
+    }
+
+    this.router.navigate(["/login"]);
+    return false;
+  }
+
+  private get isAuthPage(): boolean {
+    if (this.url.includes("login") || this.url.includes("register")) {
+      return true;
+    }
+
+    return false;
   }
 }
