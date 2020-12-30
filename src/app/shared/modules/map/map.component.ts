4218a22523f83a6de172420e57e7e72331fab0ae
jignesh zala <jigneshkumar1494@gmail.com>
2020-12-30 12:01:09 +0530

    Display map using tomtom API

--- src/app/shared/modules/map/map.component.ts
+++ src/app/shared/modules/map/map.component.ts
@@ -1,13 +1,29 @@
-import { Component, Input, OnInit } from "@angular/core";
+import { Component, Input, OnInit, ViewEncapsulation } from "@angular/core";
+import tt from "@tomtom-international/web-sdk-maps";
 
 @Component({
   selector: "app-map",
   templateUrl: "./map.component.html",
   styleUrls: ["./map.component.scss"],
+  encapsulation: ViewEncapsulation.None,
 })
 export class MapComponent implements OnInit {
-  @Input("location") location: string;
+  @Input("location") set location(location: string) {
+    this.createMap();
+  }
+  private readonly API_KEY = "vrwpROPBXRIOikeP9TG5fHYSK21sacQI";
+
   constructor() {}
 
   ngOnInit() {}
+
+  private createMap() {
+    const map = tt.map({
+      key: this.API_KEY,
+      container: "bwm-map",
+      style: "tomtom://vector/1/basic-main",
+    });
+
+    map.addControl(new tt.NavigationControl());
+  }
 }
