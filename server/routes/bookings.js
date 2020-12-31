c38914c5a37e46870b51b5880ef65fe5f907a83d
jignesh zala <jigneshkumar1494@gmail.com>
2020-12-31 18:41:41 +0530

    Get booking UTC time

--- server/routes/bookings.js
+++ server/routes/bookings.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const {
-  createBooking
+  createBooking,
+  getBookings
 } = require('../controllers/bookings');
 const {
   isUserRentalOwner
@@ -11,6 +12,8 @@ const {
   onlyAuthUser
 } = require('../controllers/users');
 
+// /api/v1/bookings?rental="8772392sad79das8d"
+router.get('', getBookings);
 router.post('', onlyAuthUser, isUserRentalOwner, createBooking);
 
 module.exports = router;
